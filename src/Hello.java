043ab2ce9a15b691873af59f4c6ae702cce9c9f7
Mr88888 <tookmr@outlook.com>
2019-10-13 11:54:30 +0800

    第一次修改(2)

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("faezds");
-        System.out.println("AAAA");
-        System.out.println("\n");
+        System.out.println("afjfk");
+        System.out.println("fsjndf");
     }
 }
