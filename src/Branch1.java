06a2c85471277ae060309e5933d4117f4baf890d
Mr88888 <tookmr@outlook.com>
2019-10-13 12:13:11 +0800

    第一次修改(2)

--- src/Branch1.java
+++ src/Branch1.java
@@ -2,6 +2,6 @@ import sun.jvm.hotspot.debugger.win32.coff.DebugVC50SSOffsetMap16;
 
 public class Branch1 {
     public static void main(String[] args) {
-        System.out.println(1);
+       
     }
 }
