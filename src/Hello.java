98e5d4d27406cf5299a22fdcbddb1365ca2bf60b
Mr88888 <tookmr@outlook.com>
2019-10-13 11:51:58 +0800

    第一次修改

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("faezds");
+        System.out.println("AAAA");
+        System.out.println("\n");
+    }
+}
