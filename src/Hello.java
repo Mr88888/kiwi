9e3811acc6a904fccdc8305d0ccd97752eac1438
Mr88888 <tookmr@outlook.com>
2019-10-13 11:58:52 +0800

    第一次修改(2)

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("afjfk");
         System.out.println("fsjndf");
+        System.out.println( "这是分支1中添加的内容");
     }
 }
